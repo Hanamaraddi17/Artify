595d9680959f6b196072f0b4e306b9298f8be5ea
Reddy <Hanamaraddibb@gmail.com>
2024-10-09 01:09:59 +0530

    contatc updated

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const db = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const artworkRoutes = require('./routes/artworkRoutes');
 const orderRoutes = require('./routes/orderRoutes');
+const contactRoutes = require('./routes/contactRoutes'); // New controller for saving to Excel
 const multer = require('multer');
 require('dotenv').config();
 
@@ -16,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/auth', authRoutes);
 app.use('/artworks', artworkRoutes);
 app.use('/orders', orderRoutes);
-
+app.use('/contact',contactRoutes)
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
