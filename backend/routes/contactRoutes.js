fff0d5299b6fb1f2839e2d96954660d560770c31
Hanamaraddi B <131801572+Hanamaraddi17@users.noreply.github.com>
2024-10-11 18:23:26 +0530

    Delete backend directory

--- backend/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { submitContactForm } = require('../controllers/contactController'); // New controller for saving to Excel
-const router = express.Router();
-
-// Route for form submission
-router.post('/', submitContactForm);
-
-module.exports = router;
